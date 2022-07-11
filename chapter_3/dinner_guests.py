a24a7d3ebc40470c3323d41aa9b7c97db682180a
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-11 15:27:14 -0600

    Add to dinner_guest list and print new invite_message

--- chapter_3/dinner_guests.py
+++ chapter_3/dinner_guests.py
@@ -17,6 +17,32 @@ print(f'{dinner_guests[0].title()}, ' + invite_message + '\n')
 print(f'{dinner_guests[1].title()}, ' + invite_message + '\n')
 print(f'{dinner_guests[2].title()}, ' + invite_message + '\n')
 
+print('I found a bigger table so I will be inviting more people. Yay!\n\n')
+
+dinner_guests.insert(0,'daphne')
+print(dinner_guests)
+
+dinner_guests.insert(2, 'kelli')
+print('\n')
+print(dinner_guests)
+
+dinner_guests.append('amanda')
+print('\n')
+print(dinner_guests)
+print('\n')
+
+print(f'{dinner_guests[0].title()}, ' + invite_message + '\n')
+print(f'{dinner_guests[1].title()}, ' + invite_message + '\n')
+print(f'{dinner_guests[2].title()}, ' + invite_message + '\n')
+print(f'{dinner_guests[3].title()}, ' + invite_message + '\n')
+print(f'{dinner_guests[4].title()}, ' + invite_message + '\n')
+print(f'{dinner_guests[5].title()}, ' + invite_message + '\n')
+
+print('Oh, no! My new table won\'t fit, so I can only invite two people!\n')
+
+
+
+
 
 
 
