9eecc69a4b8a1b04e923dbbfdc41012a4524bbd9
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-11 15:09:46 -0600

    Create dinner_guest list, print invite_message, replace corrine with andrea

---
+++ chapter_3/dinner_guests.py
@@ -0,0 +1,22 @@
+dinner_guests = ['callie', 'corrine', 'cambrea']
+invite_message = 'I would love to invite you to join me for dinner tomorrow evening at 6 pm.'
+
+print(f'{dinner_guests[0].title()}, ' + invite_message + '\n')
+
+print(f'{dinner_guests[1].title()}, ' + invite_message +'\n')
+
+print(f'{dinner_guests[2].title()}, ' + invite_message + '\n')
+
+cannot_come = 'corrine'
+dinner_guests[1] = 'andrea'
+print(cannot_come + '\n\n')
+
+print(dinner_guests)
+
+print(f'{dinner_guests[0].title()}, ' + invite_message + '\n')
+print(f'{dinner_guests[1].title()}, ' + invite_message + '\n')
+print(f'{dinner_guests[2].title()}, ' + invite_message + '\n')
+
+
+
+
